71ad0f98a3da222cf47d5510250c4afa541376b2
Hologuardian <namaeskram@gmail.com>
2013-07-26 16:49:11 -0400

    fixed texture location

--- holo/sojourn/util/Textures.java
+++ holo/sojourn/util/Textures.java
@@ -4,7 +4,7 @@ import net.minecraft.util.ResourceLocation;
 
 public class Textures 
 {
-	public static final String texLoc = "texture/";
+	public static final String texLoc = "textures/";
 	
     public static final ResourceLocation glyphTex = getResource(texLoc + "glyphs.png");
     public static final ResourceLocation essenceBar = getResource(texLoc + "essenceBar.png");
