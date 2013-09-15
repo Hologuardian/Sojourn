1430e2e35384a2f314ad069c4de76df02f843110
Hologuardian <namaeskram@gmail.com>
2013-09-15 16:41:33 -0400

    Added 4 platforms and walkways to the villages.

--- holo/sojourn/world/pythican/feature/MapGenMassiveCanopyTree.java
+++ holo/sojourn/world/pythican/feature/MapGenMassiveCanopyTree.java
@@ -13,12 +13,26 @@ import net.minecraft.world.gen.structure.StructureStart;
 public class MapGenMassiveCanopyTree extends BaseMapGen
 {
 	private static final float villageRadius = 80;
+
 	private static final int platformRadius = 20;
-	private static final int spiralWidth = 5;
-	private static final int walkwayWidth = 3;
-	private static final int villageRarity = 7;
-	private static final int spiralHeight = 15;
-	private static final int villageHeight = 20;
+
+	private static int resR = 16;
+	private static int marketR = 10;
+	private static int industrialR = 7;
+	private static int farmR = 20;
+
+	private static int resDist = 10;
+	private static int indDist = 30;
+	private static int mDist = 5;
+	private static int fDist = 15;
+
+	private static int spiralWidth = 5;
+	private static int walkwayWidth = 3;
+	private static int villageRarity = 7;
+	private static int spiralHeight = 15;
+	private static int villageHeight = 20;
+	private static int numPlatforms = 3;
+
 	private Map<Long, Boolean> treeMap = new HashMap<Long, Boolean>();
 	int treeNumber = 0;
 	int spacing;
@@ -58,12 +72,12 @@ public class MapGenMassiveCanopyTree extends BaseMapGen
 					float bx = (i - cX);
 					float by = (j - y);
 					float bz = (k - cZ);
-					
+
 					if (bx * bx + bz * bz <= (r * r + y1) && key < 65536)
 					{
 						shortArray[key] = (short) Block.wood.blockID;
 					}
-					else if(village && by < h - villageHeight && (bx * bx + bz * bz <= (r + spiralWidth) * (r + spiralWidth) + y1 && key < 65536 && inSpiral(bx, by, bz)))
+					else if(village && by < h - villageHeight && (bx * bx + bz * bz <= (r + spiralWidth) * (r + spiralWidth) + y1 && key < 65536 && inSpiral(bx, by, bz, spiralHeight)))
 					{
 						shortArray[key] = (short) Block.planks.blockID;
 					}
@@ -71,23 +85,23 @@ public class MapGenMassiveCanopyTree extends BaseMapGen
 			}
 		}
 	}
-	
-	public boolean inSpiral(float x, float y, float z)
+
+	public boolean inSpiral(float x, float y, float z, int h)
 	{
 		double theta = Math.atan2(z, x);
 		if(theta < 0)
 			theta = Math.PI * 2 + theta;
-		
-		if(fastFloor((theta / (Math.PI * 2)) * spiralHeight) == (y % spiralHeight))
+
+		if(fastFloor((theta / (Math.PI * 2)) * h) == (y % h))
 			return true;
 		return false;
 	}
-	
+
 	public int fastFloor(double x)
 	{
 		return (int) x >= 0 ? (int) x : (int)(x - 1);
 	}
-	
+
 	public void generateCanopy(long seed, int centerX, int centerZ, short[] shortArray, int x, int y, int z, int radius, int height)
 	{
 		int cX = centerX * 16 + 8;
@@ -104,9 +118,9 @@ public class MapGenMassiveCanopyTree extends BaseMapGen
 					vineFlag = true;
 				else
 					vineFlag = false;
-				
+
 				int groundHeight = getTopSolidOrLiquidBlock((int)(i - x), (int)(k - z), shortArray);
-				
+
 				for(float j = y + height; j >= 0 && j < 256; --j)
 				{
 					int key = hashCoord((int)(i - x), (int)j, (int)(k - z));
@@ -117,7 +131,7 @@ public class MapGenMassiveCanopyTree extends BaseMapGen
 					{
 						shortArray[key] = (short) Block.leaves.blockID;
 					}
-					
+
 					if(vineFlag && by <= lowerCanopyY(bx, by, bz, radius, height, lDiv) && key < 65536 &&  j > groundHeight + 10
 							&& bx * bx + bz * bz <= radius * radius - 25)
 					{
@@ -173,7 +187,7 @@ public class MapGenMassiveCanopyTree extends BaseMapGen
 	{
 		float rDiv = (rand.nextFloat() * 0.05F + 1.0F);
 		float hDiv = (rand.nextFloat() * 0.05F + 1.0F);
-		
+
 		generateTrunk(seed, centerX, centerZ, shortArray, x, y, z, (int)(baseRadius * rDiv), (int)(baseHeight * hDiv), village);
 		generateCanopy(seed, centerX, centerZ, shortArray, x, y + (int)(baseHeight * hDiv), z, (int)(baseCanopyRadius * rDiv), (int)(baseCanopyHeight * hDiv));
 		if(village)
@@ -201,18 +215,50 @@ public class MapGenMassiveCanopyTree extends BaseMapGen
 					float bx = (i - cX);
 					float by = (j - y);
 					float bz = (k - cZ);
-					
-					if (bx * bx + bz * bz <= (platformRadius * platformRadius) && (bx * bx + bz * bz >= (9 + spiralWidth) * (9 + spiralWidth)) && key < 65536)
+					if(key < 65536)
 					{
-						shortArray[key] = (short) Block.planks.blockID;
+						if (bx * bx + bz * bz <= (platformRadius * platformRadius) && (bx * bx + bz * bz >= (9 + spiralWidth) * (9 + spiralWidth)))
+						{
+							shortArray[key] = (short) Block.planks.blockID; // Center platform
+						}
+						else if((bx - (platformRadius + resR + resDist)) * (bx - (platformRadius + resR + resDist)) + (bz) * (bz) <= (resR * resR))
+						{
+							shortArray[key] = (short) Block.planks.blockID; // +x residential
+						}
+						else if((bx + (platformRadius + industrialR + indDist)) * (bx + (platformRadius + industrialR + indDist)) + (bz) * (bz) <= (industrialR * industrialR))
+						{
+							shortArray[key] = (short) Block.planks.blockID; // -x industrial
+						}
+						else if((bz - (platformRadius + marketR + mDist)) * (bz - (platformRadius + marketR + marketR)) + (bx) * (bx) <= (marketR * marketR))
+						{
+							shortArray[key] = (short) Block.planks.blockID; // +z market
+						}
+						else if((bz + (platformRadius + farmR + fDist)) * (bz + (platformRadius + farmR + fDist)) + (bx) * (bx) <= (farmR * farmR))
+						{
+							shortArray[key] = (short) Block.planks.blockID; // -z farm
+						}
+						else 
+						{
+							float abx = Math.abs(bx);
+							float abz = Math.abs(bz);
+							if(abz <= 1)
+							{
+								if(abx >= platformRadius && (bx <= (platformRadius + resR + resDist)) && (bx >= -(platformRadius + industrialR + indDist)))
+								{
+									shortArray[key] = (short) Block.planks.blockID; // residential and industrial walkways
+								}
+							}
+							else if(abx <= 1)
+							{
+								if(abz >= platformRadius && (bz <= (platformRadius + marketR + mDist)) && (bz >= -(platformRadius + farmR + fDist)))
+								{
+									shortArray[key] = (short) Block.planks.blockID; // market and farm walkways
+								}
+							}
+						}
 					}
 				}
 			}
 		}
-	}
-	
-	public void genPlatform(World world, int centerX, int centerZ, short[] shortArray, int x, int y, int z, int r, int pX, int pY, int pZ)
-	{
-		
 	}
 }
